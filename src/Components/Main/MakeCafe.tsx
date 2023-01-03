640dd42978b5683b278ed16cab347552c35dad47
jongin12 <jongin981224@gmail.com>
2023-01-03 14:46:02 +0900

    카페 생성 페이지 작동

--- src/Components/Main/MakeCafe.tsx
+++ src/Components/Main/MakeCafe.tsx
@@ -6,12 +6,55 @@ import { Link } from 'react-router-dom';
 const makeCafe = () => {
 
   let sessionStorage = window.sessionStorage
+  console.log(sessionStorage.login);
 
-  console.log(sessionStorage)
+  const [name, setName] = useState("")
+
+  const handleChange = (e:any) => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    let nameCheck =  /^[a-zA-Z0-9]*$/
+    //! 영어와 숫자만 사용 가능
+    if(nameCheck.test(name) && name !== ''){
+      fetch("http://localhost:4625/makeCafe",{
+        method: "POST",
+        mode: 'cors',
+        body: JSON.stringify({
+          name: name,
+          manager: sessionStorage.id,
+        }),
+      })
+      .then((res)=>res.json())
+      .then((res)=>{
+        console.log(res)
+      })
+    }
+  }
 
   return (
     <>
       <h3>카페 만들기</h3>
+      {
+        sessionStorage.login &&
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <input
+            type="text"
+            name='id'
+            value={name}
+            onChange={handleChange}
+            placeholder="카페명"
+            autoComplete="off"
+          />
+          <button type="submit">생성</button>
+        </form>
+      }
+      {
+        !sessionStorage.login &&
+        <p>로그인이 필요합니다.</p>
+      }
     </>
   )
 }
